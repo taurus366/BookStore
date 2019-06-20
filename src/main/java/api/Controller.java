9a0335e19d474a321719e5ec77b273656363315d
taurus366 <taurus.ali47@gmail.com>
2019-06-20 13:05:46 +0300

    added GET,POST methods ,added indexes html's files

--- src/main/java/api/Controller.java
+++ src/main/java/api/Controller.java
@@ -1,7 +1,7 @@
 package api;
 
 import javax.ws.rs.FormParam;
-
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
@@ -11,11 +11,26 @@ public class Controller {
 
 	@POST
 	@Path("/register")
-	public String GetArticleBYTitle(@FormParam("name") String name,@FormParam("password") String password,@FormParam("email") String email) {
+	public String doRegister(@FormParam("name") String name,@FormParam("secondname") String secondname,@FormParam("password") String password,@FormParam("email") String email,@FormParam("address") String address) {
 
 		
 
 		return name + password + email;
 
 	}
+	
+	@GET
+	@Path("/login")
+	public String doLogin(@FormParam("email")String email,@FormParam("password")String password) {
+		
+		
+		return null;
+	}
+	
+	@POST
+	@Path("/order")
+	public String doOrder(@FormParam("name")String name,@FormParam("secondname")String secondname,@FormParam("phone")String phone) {
+		return null;
+	}
+	
 }
